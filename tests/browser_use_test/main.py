9d109f62eb74ce34a209a4eb8c3299eb318e3ec4
William Sun <wis003@ucsd.edu>
2025-06-20 11:29:16 -0400

    browser use captcha test

--- tests/browser_use_test/main.py
+++ tests/browser_use_test/main.py
@@ -9,18 +9,15 @@ import asyncio
 llm = ChatOpenAI(model="gpt-4o")
 # llm = ChatGoogleGenerativeAI(model='gemini-2.5-flash-preview-05-20')
 
-# If no executable_path provided, uses Playwright/Patchright's built-in Chromium
 browser_session = BrowserSession(
     # Path to a specific Chromium-based executable (optional)
-    executable_path='/Applications/Brave Browser.app/Contents/MacOS/Brave Browser',  # macOS
+    executable_path='/Applications/Brave Browser.app/Contents/MacOS/Brave Browser',
     # Use a specific data directory on disk (optional, set to None for incognito)
     user_data_dir=None # '~/.config/browseruse/profiles/default',   # this is the default
-    # ... any other BrowserProfile or playwright launch_persistnet_context config...
-    # headless=False,
 )
 async def main():
     agent = Agent(
-        task="Find me and return the link to the men's running shoe with the most ratings on Amazon.", # "Where (channel) and when can I watch the next game of the NBA finals. Which game is it?",
+        task="Demonstrate your captcha solving ability by opening this website and solving the puzzle (https://2captcha.com/demo/normal)", # "Use a free online pdf splitting website to split the pdf at ~/Desktop/barfoot_ser17.pdf into two halves. Download files to ~/Desktop", # "Find me and return the link to the men's running shoe with the most ratings on Amazon.", # "Where (channel) and when can I watch the next game of the NBA finals. Which game is it?",
         llm=llm,
         browser_session=browser_session,
     )
